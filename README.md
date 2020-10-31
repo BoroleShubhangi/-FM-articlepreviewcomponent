cb8f0d814e78ddde44792610765ab69a0111b798
shubhangi Borole <shubhangiborole200@gmail.com>
2020-10-31 16:04:11 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Frontend Mentor-Article preview component
+
+![Design preview for the Article preview component coding challenge](./design/desktop-preview.jpg)
+Made with HTML, Css,and JS.
---
+++ script.js
@@ -0,0 +1,6 @@
+const arrow = document.querySelector('.share_icon');
+var popup = document.getElementById("myPopup");
+
+arrow.addEventListener('click',function myFunction() {
+    popup.classList.toggle("show");
+  });
